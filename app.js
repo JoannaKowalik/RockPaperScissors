1f05e69282c9ce33f9088b3c7079fbfdc50807e0
JoannaKowalik <joanna.kowalik@icloud.com>
2024-11-19 21:45:45 +0000

    3 rounds functionality added

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let count = 0;
+let userScore = 0;
+let compScore = 0;
 function playGame(event) {
   let userInput = event.target.id;
   console.log(userInput);
@@ -7,30 +10,33 @@ function playGame(event) {
 
   if (userInput == compInput) {
     msg = "It's a tie!";
-  } else if (userInput == "rock") {
-    if (compInput == "paper") {
-      msg = "You lose!";
-    } else {
-      msg = "You win!";
-    }
-  } else if (userInput == "paper") {
-    if (compInput == "rock") {
-      msg = "You win!";
-    } else {
-      msg = "You lose!";
-    }
+  } else if (
+    (userInput == "rock" && compInput == "scissors") ||
+    (userInput == "paper" && compInput == "rock") ||
+    (userInput == "scissors" && compInput == "paper")
+  ) {
+    msg = "You win!";
+    userScore = userScore + 1;
+    console.log(userScore);
   } else {
-    if (compInput == "rock") {
-      msg = "You lose!";
-    } else {
-      msg = "You win!";
-    }
+    msg = "You lose!";
+    compScore = compScore + 1;
+    console.log(compScore);
   }
   document.getElementById("user").innerHTML = "Your choice: " + userInput;
   document.getElementById("comp").innerHTML = "Computer choice: " + compInput;
   document.getElementById("msg").innerHTML = msg;
   console.log(msg);
-}
+  count++;
 
-//   });
-// }
+  if (count >= 3 || compScore >= 2 || userScore >= 2) {
+    document.getElementById("rock").style.display = "none";
+    document.getElementById("paper").style.display = "none";
+    document.getElementById("scissors").style.display = "none";
+    document.getElementById("user").style.display = "none";
+    document.getElementById("comp").style.display = "none";
+    document.getElementById("msg").style.display = "none";
+    document.getElementById("gameover").innerHTML =
+      "Game over! You score: " + userScore + " Computer scores: " + compScore;
+  }
+}
